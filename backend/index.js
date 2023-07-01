c28fa85368351762c1d5c344a66041caaaf26105
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-07-01 09:13:53 +0530

    Updated Some Codes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const server = require("./server.js");
 
 if (process.env.NODE_ENV === "test") {
-  server.listen(4401, () => {
-    console.log("Capstone Project Backend is running on http://localhost:4401");
+  server.listen(3000, () => {
+    console.log("Capstone Project Backend is running on http://localhost:3000");
   });
 } else {
   server.listen(8080, () => {
